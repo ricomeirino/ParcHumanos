494cecb984baa542ec4719831c89e5caa08e63fa
Ricardo Meirino <ricardo.meirino@gmail.com>
2014-10-17 17:09:46 -0300

    Teste add classe

---
+++ PARCHumanos/src/main/java/Controller/Teste.java
@@ -0,0 +1,21 @@
+package Controller;
+
+public class Teste {
+
+	public int codigo;
+	public String nome;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
